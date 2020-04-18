538395602bbedcd669991e6e7cbd79750bb9db39
Cat Core <arthur.koalas@free.fr>
2020-04-19 00:28:09 +0200

    Fix crash when other mods registering new buckets are present

--- src/main/java/io/github/vampirestudios/spontaneousbucketing/client/styles/Styles.java
+++ src/main/java/io/github/vampirestudios/spontaneousbucketing/client/styles/Styles.java
@@ -2,9 +2,8 @@ package io.github.vampirestudios.spontaneousbucketing.client.styles;
 
 public class Styles {
 
-    public static Style VANILLA;
+    public static Style VANILLA = new Style("vanilla");
 
     public static void init() {
-        VANILLA = new Style("vanilla");
     }
 }
