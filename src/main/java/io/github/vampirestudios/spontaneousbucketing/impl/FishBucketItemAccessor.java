2a0896ec6ee34f2c81f530e1425dbf058a18ac61
Cat Core <arthur.koalas@free.fr>
2020-04-22 18:27:59 +0200

    Fix Fish Buckets names

---
+++ src/main/java/io/github/vampirestudios/spontaneousbucketing/impl/FishBucketItemAccessor.java
@@ -0,0 +1,9 @@
+package io.github.vampirestudios.spontaneousbucketing.impl;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.FishEntity;
+
+public interface FishBucketItemAccessor {
+
+    EntityType<?> getEntityType();
+}
