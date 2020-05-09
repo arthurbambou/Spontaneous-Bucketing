4e282878a50acf11e34716ebd49160700b57fdcb
Cat Core <arthur.koalas@free.fr>
2020-05-09 17:35:00 +0200

    Fix runtime resource pack

--- src/main/java/io/github/vampirestudios/spontaneousbucketing/mixin/MixinGameOptions.java
+++ src/main/java/io/github/vampirestudios/spontaneousbucketing/mixin/MixinGameOptions.java
@@ -2,21 +2,19 @@ package io.github.vampirestudios.spontaneousbucketing.mixin;
 
 import io.github.vampirestudios.spontaneousbucketing.client.BucketColorManager;
 import io.github.vampirestudios.spontaneousbucketing.client.BucketTextureManager;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.RunArgs;
-import net.minecraft.client.tutorial.TutorialManager;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.resource.ClientResourcePackProfile;
+import net.minecraft.resource.ResourcePackManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TutorialManager.class)
-public class MixinTutorialManager {
+@Mixin(GameOptions.class)
+public class MixinGameOptions {
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void beforeResourcePackLoad(MinecraftClient minecraftClient, CallbackInfo ci) {
+    @Inject(method = "addResourcePackProfilesToManager", at = @At("HEAD"))
+    private void registerResourcePack(ResourcePackManager<ClientResourcePackProfile> manager, CallbackInfo ci) {
         BucketTextureManager.init();
         BucketColorManager.init();
     }
